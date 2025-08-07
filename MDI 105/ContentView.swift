2ceeb9a4eb33b3562770da4239255f5ff40eb2dd
RoyDMAP <roy.dimapilis@yahoo.com>
2025-08-06 19:03:53 -0700

    Initial Commit

---
+++ MDI 105/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MDI 105
+//
+//  Created by Roy Dimapilis on 8/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MDI 105/MDI_105App.swift
@@ -0,0 +1,17 @@
+//
+//  MDI_105App.swift
+//  MDI 105
+//
+//  Created by Roy Dimapilis on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MDI_105App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
